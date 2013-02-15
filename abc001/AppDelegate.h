f75552eb27f582dd3a942c881657b326333db8f4
kojinao <kojinao@lilac.ocn.ne.jp>
2013-02-15 19:14:11 +0900

    Initial Commit

---
+++ abc001/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  abc001
+//
+//  Created by Naomi Kojima on 2013/02/15.
+//  Copyright (c) 2013年 Naomi Kojima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ abc001/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  abc001
+//
+//  Created by Naomi Kojima on 2013/02/15.
+//  Copyright (c) 2013年 Naomi Kojima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
